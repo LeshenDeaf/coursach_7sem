0deceb048e678e073875a4ac49dd20759527aa8a
leshen <lexuar2000@gmail.com>
2022-03-08 16:42:27 +0300

    Added owner to field

--- database/migrations/2022_02_06_133422_create_fields_table.php
+++ database/migrations/2022_02_06_133422_create_fields_table.php
@@ -15,9 +15,13 @@ class CreateFieldsTable extends Migration
     {
         Schema::create('fields', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id')->unsigned();
             $table->string('label')->unique();
             $table->string('name')->unique();
             $table->smallInteger('type');
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
 
             $table->timestamps();
         });
