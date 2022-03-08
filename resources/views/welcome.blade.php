9ced8b6b4e919c43aefe7023ac96af252b9d9522
leshen <lexuar2000@gmail.com>
2022-03-08 16:48:51 +0300

    Bugfix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <h1>Test</h1>
-    {{ \App\Models\User::find(1)->with('roles')->where('id', 11)->get() }}
+    {{ \App\Models\User::take(1)->with('roles')->where('id', 11)->get() }}
     <br>
     <br>
-    {{ \App\Models\Role::find(1)->with('users')->where('id', 1)->get() }}
+    {{ \App\Models\Role::take(1)->with('users')->where('id', 1)->get() }}
 @endsection
