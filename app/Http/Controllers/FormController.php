526c3cee531b3f4564f04e54805648938296a36f
leshen <lexuar2000@gmail.com>
2022-03-09 15:44:52 +0300

    Now you can change form

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -23,6 +23,13 @@ class FormController extends Controller
         return view('home.forms.create');
     }
 
+    public function edit(int $id)
+    {
+        $form = Form::with('fields')->findOrFail($id);
+
+        return view('home.forms.edit', compact('form'));
+    }
+
     public function store(Request $request)
     {
         $request->validate([
@@ -39,6 +46,23 @@ class FormController extends Controller
         $form->fields()->sync(
             $request->input('fields')
         );
+
+        return redirect(route('home.forms.index'));
+    }
+
+    public function update(Request $request, Form $form)
+    {
+        $request->validate([
+            'name' => ['required', 'max:255'],
+            'fields' => ['required', 'max:255', 'array'],
+            'fields.*' => ['integer']
+        ]);
+
+        $form->update([
+            'name' => $request->input('name'),
+        ]);
+
+        $form->fields()->sync($request->input('fields'));
 
         return redirect(route('home.forms.index'));
     }
