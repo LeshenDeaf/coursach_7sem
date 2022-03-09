7aa81211787a501748e10c6eddb614edfadb10ea
leshen <lexuar2000@gmail.com>
2022-03-09 14:30:24 +0300

    Fixed delete buttons and from action route

--- resources/views/home/forms/create.blade.php
+++ resources/views/home/forms/create.blade.php
@@ -8,7 +8,7 @@
 
     <div class="items-center">
         <div class="rounded-xl bg-white shadow-lg w-1/2 mx-auto px-5 py-6">
-            <form method="post" action="{{ route('admin.users.store') }}">
+            <form method="post" action="{{ route('home.forms.store') }}">
                 @csrf
                 <a href="{{ route('home.forms.index') }}"
                    class="inline-block align-baseline font-bold text-sm text-gray-500 hover:text-blue-800 hover:bg-gray-50 px-3 py-1 rounded-xl"
@@ -17,7 +17,9 @@
                 </a>
 
                 @include('admin.users.partials.input', ['label' => 'Name', 'name' => 'name', 'isRequired' => true])
-                @include('home.forms.partials.list_input', ['label' => 'Fields', 'fields' => [new \App\Models\Field(['label' => 'When paid', 'type' => 'date'])]])
+                @include('home.forms.partials.list_input', ['label' => 'Fields', 'fields' => []])
+
+{{--                [new \App\Models\Field(['label' => 'When paid', 'type' => 'date', 'canDelete' => false])]--}}
 
                 <button class="rounded-xl bg-blue-500 text-white w-full py-2 my-2 hover:bg-blue-600 transition shadow-lg hover:shadow-none">
                     {{ __('Submit') }}
