506ef266eab3ee842e1397e6c63bf1f4658e5efc
leshen <lexuar2000@gmail.com>
2022-03-09 14:41:41 +0300

    Yay, now you can save a form

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -22,4 +22,24 @@ class FormController extends Controller
     {
         return view('home.forms.create');
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:255'],
+            'fields' => ['required', 'max:255', 'array'],
+            'fields.*' => ['integer']
+        ]);
+
+        $form = Form::create([
+            'name' => $request->input('name'),
+            'user_id' => auth()->user()->id,
+        ]);
+
+        $form->fields()->sync(
+            $request->input('fields')
+        );
+
+        return redirect(route('home.forms.index'));
+    }
 }
