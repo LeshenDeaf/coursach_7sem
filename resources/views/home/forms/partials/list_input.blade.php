37c28b860ff7e801ad64f0e07e512bcebdc81c0e
leshen <lexuar2000@gmail.com>
2022-03-09 15:52:27 +0300

    Fixed bug and added field label output instead of its type id

--- resources/views/home/forms/partials/list_input.blade.php
+++ resources/views/home/forms/partials/list_input.blade.php
@@ -4,9 +4,9 @@
         @foreach($fields ?? [] as $field)
         <div class="li_field" name="{{ $field->id }}">
             <div class="field_info">
-                {{ $field->label }} <span class="text-gray-600">({{ $field->type }})</span>
+                {{ $field->label }} <span class="text-gray-600">({{ \App\Models\Field::getTypeLabel($field->type) }})</span>
             </div>
-            @if ($canDelete ?? false)
+            @if ($canDelete ?? true)
                 <div class="delete_field"><span class="x_del">x</span></div>
             @endif
             <input type="hidden" name="fields[]" value="{{ $field->id }}">
