e17251adebe450c7df9010dabbe9ab6386bd94c5
leshen <lexuar2000@gmail.com>
2022-03-14 20:22:24 +0300

    Now the label is prettier and in placeholder user is able to see the input's type

--- resources/views/home/answers/partials/input.blade.php
+++ resources/views/home/answers/partials/input.blade.php
@@ -1,20 +1,23 @@
 <?php
 $fieldType = \App\Models\Field::getTypeName($field->type);
+$fieldTypeLabel = \App\Models\Field::getTypeLabel($field->type);
 ?>
 
 <div class="text-slate-600 p-3">
-    <label class="flex">
-        {{ $field->label }}:
+    <label class="">
+        <span class="block">{{ $field->label }}:</span>
+
         <input class="@error( $field->name ?? '' ) is-invalid @enderror input_{{ $fieldType }} w-full text-slate-800 py-1 px-2 hover:text-blue-900 hover:bg-gray-100 rounded-lg cursor-pointer border-transparent focus:border-transparent focus:ring-0 border-none focus:outline-none"
                value="{{ $value ?? '' }}"
-               placeholder="Not filled"
+{{--               placeholder="Not filled"--}}
                name="{{ $field->name }}"
                type="{{ $type ?? 'text' }}"
+               placeholder="{{ $fieldTypeLabel }}"
                required
                autocomplete({{ $field->name }})
         >
         @error( $field->name ?? '' )
-        <span class="invalid-feedback" role="alert">
+            <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
         @enderror
