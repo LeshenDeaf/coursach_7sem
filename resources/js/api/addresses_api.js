c539f3b96e85b1ec0ff51932f581879d04f94b6a
leshen <lexuar2000@gmail.com>
2022-03-18 15:47:25 +0300

    All code for addresses autocompletion

---
+++ resources/js/api/addresses_api.js
@@ -0,0 +1,77 @@
+const Api = require('./api');
+
+class AddresesApi extends Api {
+    constructor () {
+        super('/kladr-api');
+    }
+
+    get (succCallback, errCallback, address) {
+        return $.ajax({
+            url: this.url,
+            method: "POST",
+            data: 'query=' + address,
+        })
+            .done(succCallback)
+            .fail(
+                (jqXHR, textStatus) => errCallback("Request failed: " + textStatus)
+            );
+    }
+}
+
+const fillSuggestions = (response, input) => {
+    response = JSON.parse(response);
+
+    const parent = input.parents('div').eq(0);
+
+    if (parent.find('.suggestions').length <= 0) {
+        parent.append('<div class="suggestions"></div>')
+    }
+
+    const suggestions = parent.find('.suggestions');
+
+    suggestions.html('');
+
+    if (input.val() === response.suggestions[0].value && response.suggestions.length === 1) {
+        suggestions.remove();
+        return;
+    }
+
+    for (const suggestion of response.suggestions) {
+        suggestions.append(`<div class="suggestion">${suggestion.value}</div>`)
+    }
+}
+
+const addressesApi = new AddresesApi();
+
+let timeout;
+
+let oldAddress = '';
+
+$(document).on('input', 'input[name="address"]', function () {
+    const address = $(this).val().trim();
+    const input = $(this);
+
+    if (!address || address === oldAddress) {
+        return;
+    }
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+        addressesApi.get(response => fillSuggestions(response, input), alert, address);
+        oldAddress = address;
+    },200);
+})
+
+$(document).on('click', '.suggestion', function () {
+    const input = $(this).parent().parent().find('input[name="address"]');
+
+    if (input.length <= 0) {
+        return;
+    }
+
+    input.val($(this).text());
+    input.trigger('input');
+});
+
+module.exports = addressesApi;
