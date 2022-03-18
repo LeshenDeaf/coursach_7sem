c81450af7cc387b95843b39c406872f57a02f2ef
leshen <lexuar2000@gmail.com>
2022-03-18 09:34:31 +0300

    Fields are grouped now and changed text if no fields in form exist

--- resources/views/home/answers/create.blade.php
+++ resources/views/home/answers/create.blade.php
@@ -6,7 +6,12 @@
     @if(!$form)
         Form is not defined
     @elseif(empty($form->fields->toArray()))
-        Form fields does not exist
+        <h1 class="text-center text-black text-xl my-8">Form is empty <br><span class="text-gray-600">(no fields attached for it)</span></h1>
+        <a href="{{ redirect()->back()->getTargetUrl() }}"
+           class="mx-auto w-fit block font-bold text-xl text-gray-500 hover:text-blue-800 hover:bg-gray-50 px-3 py-1 rounded-xl"
+        >
+            Back
+        </a>
     @else
         @include("partials.header_text", ['text' => $form->name, 'classes' => 'text-center'])
         @include("partials.alerts.alert")
@@ -17,15 +22,27 @@
                     @method('POST')
                     @csrf
 
-                    <a href="{{ back() }}"
+                    <a href="{{ redirect()->back()->getTargetUrl() }}"
                        class="inline-block align-baseline font-bold text-sm text-gray-500 hover:text-blue-800 hover:bg-gray-50 px-3 py-1 rounded-xl"
                     >
                         Back
                     </a>
 
+                    @foreach(\App\Models\Field::$types as $typeName => $type)
+                        @if(!isset($form->fields[$type]))
+                            @continue
+                        @endif
+                        <div class="wrap rounded-xl border border-gray-200 my-4 py-2">
+                            <div class="wrap_header text-center text-lg select-none">{{ \App\Models\Field::getTypeLabel($type) }}s</div>
 
-                    @foreach($form->fields as $field)
-                        @include('home.answers.partials.input', ['field' => $field])
+                            <div class="wrap_body hidden">
+                                @foreach($form->fields[$type] as $field)
+                                    @include('home.answers.partials.input', ['field' => $field])
+                                @endforeach
+                            </div>
+
+                        </div>
+
                     @endforeach
 
                     <button class="rounded-xl bg-blue-500 text-white w-full py-2 my-2 hover:bg-blue-600 transition shadow-lg hover:shadow-none">
