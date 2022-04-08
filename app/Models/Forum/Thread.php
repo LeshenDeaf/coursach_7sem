b8d141fb82e368e2ae7c8816a1d0bd15d8d9aeb8
leshen <lexuar2000@gmail.com>
2022-04-08 14:48:05 +0300

    Created models for forum

---
+++ app/Models/Forum/Thread.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models\Forum;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Thread extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id', 'user_id',
+        'title', 'text',
+    ];
+
+    protected static function boot() {
+        parent::boot();
+
+        static::creating(fn ($thread) => $thread->slug = Str::slug($thread->title));
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(ThreadComment::class);
+    }
+
+}
