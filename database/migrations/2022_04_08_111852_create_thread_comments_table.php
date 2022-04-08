dca215cd7f97ab821625076f1fdbfb16a21e42dd
leshen <lexuar2000@gmail.com>
2022-04-08 14:36:34 +0300

    Created migrations for forum

---
+++ database/migrations/2022_04_08_111852_create_thread_comments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thread_comments', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('thread_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->bigInteger('parent_id')->index()->nullable();
+
+            $table->text('body');
+
+            $table->foreign('parent_id')
+                ->references('id')->on('thread_comments')
+                ->cascadeOnDelete()->cascadeOnUpdate();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('thread_comments');
+    }
+}
