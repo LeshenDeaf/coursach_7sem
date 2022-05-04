52547487a1b75a5345dbfd0c4a78595bca09a06a
leshen <lexuar2000@gmail.com>
2022-05-04 09:17:50 +0300

    Added category seeder to dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(CategorySeeder::class);
+
         \App\Models\User::factory(10)
             ->hasAttached(
                 Role::factory()->count(1),
