e8dab79e8f166364e4b7ebbde30ddf2635f8b722
leshen <lexuar2000@gmail.com>
2022-05-06 10:41:28 +0300

    Refactoring

--- resources/views/home/forum/partials/search.blade.php
+++ resources/views/home/forum/partials/search.blade.php
@@ -1,12 +1,12 @@
-<div class="sticky top-4 mx-auto w-[650px]">
-    <div class="absolute  w-full">
+<div class="sticky top-4 mx-auto w-[750px] mb-3">
+    <div class="absolute  w-full py-2 px-3 border shadow-lg rounded-lg bg-white">
         <div class="flex items-start content-start search">
-            @include('home.forum.partials.address_input', ['label' => 'Address', 'autocomplete' => false, 'isRequired' => true,])
+            @include('home.forum.partials.address_input', ['label' => 'Address', 'autocomplete' => false, 'isRequired' => true, 'value' => $address ?? ''])
 
             <div class="">
                 <div class="ml-4 mt-6">
                     <button
-                        class="find px-6 py-2 border bg-white border-gray-100 text-black rounded-xl shadow-lg hover:bg-gray-100 duration-100 transition-all"
+                        class="find px-6 py-2 bg-blue-500 rounded-xl text-white hover:bg-blue-700 duration-100 transition-all"
                         type="button"
                     >
                         Найти
