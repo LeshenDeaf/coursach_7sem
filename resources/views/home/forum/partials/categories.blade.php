8273f34cd3a303582f510345160cbecad0ba5b33
leshen <lexuar2000@gmail.com>
2022-05-06 10:41:13 +0300

    Refactoring

--- resources/views/home/forum/partials/categories.blade.php
+++ resources/views/home/forum/partials/categories.blade.php
@@ -4,21 +4,17 @@
     </div>
 
     <div class="wrap_body hidden px-8 pt-2">
-        <div class="text-sm font-bold select-none mb-2 border-b py-2 border-b-gray-100 hover:border-b-blue-200">
-            <span class="hover:text-blue-400 transition-all">
-                <a href="{{ route('home.forum.index') }}">
-                    All
-                </a>
-            </span>
+        <div class="text-sm font-bold select-none mb-2 border-b py-2 border-b-gray-100 hover:border-b-blue-200 hover:text-blue-400 transition-all">
+            <a class="block" href="{{ route('home.forum.index') }}">
+                All
+            </a>
         </div>
 
         @foreach($categories as $category)
-            <div class="text-sm font-bold select-none mb-2 border-b py-2 border-b-gray-100 hover:border-b-blue-200">
-                <span class="hover:text-blue-400 transition-all">
-                    <a href="{{ route('home.forum.category', $category->slug) }}">
-                        {{ $category->name }}
-                    </a>
-                </span>
+            <div class="text-sm font-bold select-none mb-2 border-b py-2 border-b-gray-100 hover:border-b-blue-200 hover:text-blue-400 transition-all">
+                <a class="block" href="{{ route('home.forum.category', $category->slug) }}">
+                    {{ $category->name }}
+                </a>
             </div>
         @endforeach
     </div>
