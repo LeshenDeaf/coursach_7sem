33d341c0fe70753a7ab62b827af26c13544ed75f
leshen <lexuar2000@gmail.com>
2022-05-06 10:19:00 +0300

    Search is now working

--- resources/js/components/search.js
+++ resources/js/components/search.js
@@ -1,11 +1,23 @@
+const Api = require('../api/api');
 
+const api = new Api('/home/forum/address/search');
+
+const redirectToAddress = response => {
+    window.location.replace(
+        '/home/forum/address/' + response.address_id
+    );
+}
 
 $('.search .find').on('click', function (e) {
     e.preventDefault();
 
+    const fd = new FormData();
     const input = $(this).parent().parent().parent().find('input');
+    if (!input || !input.val().trim()) {
+        return;
+    }
 
+    fd.append('address', input.val().trim());
 
-
-    console.log(input);
+    api.find(redirectToAddress, console.log, fd, false);
 })
