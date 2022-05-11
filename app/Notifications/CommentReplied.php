0f4076b90f5be6d520498838426fc867e2dc633a
leshen <lexuar2000@gmail.com>
2022-05-11 12:44:16 +0300

    Created event for replying to a comment.

--- app/Notifications/CommentReplied.php
+++ app/Notifications/CommentReplied.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\Models\Forum\ThreadComment;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -11,51 +12,45 @@ class CommentReplied extends Notification
 {
     use Queueable;
 
+    public ThreadComment $comment;
+
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(ThreadComment $comment)
     {
-        //
+        $this->comment = $comment;
     }
 
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['database'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
+    public function toDatabase($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        $user = $this->comment->user;
+        $arr = $this->comment->toArray();
+        $arr["_message"] = "{$user->name} replied to your comment";
+
+        return $arr;
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function toArray($notifiable)
     {
-        return [
-            //
-        ];
+        $user = $this->comment->user;
+        $arr = $this->comment->toArray();
+        $arr["_message"] = "{$user->name} replied to your comment";
+
+        return $arr;
     }
+
 }
