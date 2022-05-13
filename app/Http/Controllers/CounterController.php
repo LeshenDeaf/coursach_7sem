e37820bdeb3ca39227492b3986038b7a4138f8ff
leshen <lexuar2000@gmail.com>
2022-05-13 09:24:12 +0300

    Created method getFullInfo and refactored code.
    getFullInfo returns either empty array (no info found) either all data about counter

--- app/Http/Controllers/CounterController.php
+++ app/Http/Controllers/CounterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Counter;
 use Illuminate\Http\Request;
 
 class CounterController extends Controller
@@ -10,7 +11,6 @@ class CounterController extends Controller
 
     public function search(Request $request)
     {
-        list($registerType,  $factoryNumber) = [$request->input('register_type'), $request->input('factory_number')];
         $curl = curl_init();
 
         $fields = [
@@ -24,12 +24,12 @@ class CounterController extends Controller
             'sticker_num'
         ];
 
-        curl_setopt_array($curl, array(
+        curl_setopt_array($curl, [
             CURLOPT_URL => config('api.fgis.address.search')
-                . '?fq=mi.mitnumber:*' . $this->addSlashes($registerType)
-                . '*&fq=mi.number:*' . $this->addSlashes($factoryNumber)
-                . '*&q=*&fl=' . implode(',', $fields)
-                . '&sort=verification_date desc,org_title asc&rows=' . static::LIMIT
+                . '?fq=mi.mitnumber:' . urlencode($request->input('register_type'))
+                . '&fq=mi.number:' . urlencode($request->input('factory_number'))
+                . '&q=*&fl=' . implode(',', $fields)
+                . '&sort=verification_date%20desc,org_title%20asc&rows=' . static::LIMIT
                 . '&start=0',
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
@@ -38,20 +38,89 @@ class CounterController extends Controller
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
+            CURLOPT_HTTPHEADER => [
                 'Accept: application/json, text/plain, */*',
 //                'Cookie: session-cookie=16ee023b43d69554ae9509d918991a24fbe9d730fa952afe331bce3162108c224fbf49b99dafe6cf8fca84ed334f8be4'
-            ),
-        ));
+            ],
+        ]);
 
-        $response = json_decode(curl_exec($curl));
+        $response = json_decode(curl_exec($curl), true);
+
+        if (!$response || !$response['response']['docs']) {
+            return [];
+        }
 
         curl_close($curl);
         return $response['response']['docs'];
     }
 
+    public function getFullInfo(string $vriId)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => config('api.fgis.address.get') . $vriId,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => [
+                'Accept: application/json, text/plain, */*',
+            ],
+        ]);
+
+        $response = json_decode(curl_exec($curl), true);
+
+        if (!$response) {
+            return [];
+        }
+
+        curl_close($curl);
+        return $response;
+    }
+
+    public function store(Request $request)
+    {
+        //        $request->input('register_type'), $request->input('factory_number');
+        $request->validate([
+            'address_id' => ['integer', 'required'],
+            'register_type' => ['string', 'required'],
+            'factory_number' => ['string', 'required'],
+        ]);
+
+        $docs = $this->search($request);
+
+        if (!$docs) {
+            return response()->json(['error' => 'No counters found'], 404);
+        }
+
+        $counter = $this->getFullInfo($docs[0]['vri_id']);
+
+        if (!$counter || !$counter['result']) {
+            return response()->json(['error' => 'Counter not found'], 404);
+        }
+
+        $miData = $counter['result']['miInfo']['singleMI'];
+        $verificationData = $counter['result']['vriInfo'];
+
+        return Counter::create([
+            'address_id' => $request->input('address_id'),
+            'vri_id' => $docs[0]['vri_id'],
+            'registration_type_number' => $miData['mitypeNumber'],
+            'modification_name' => $miData['modification'],
+            'factory_number' => $miData['manufactureNum'],
+            'release_year' => $miData['manufactureYear'] ?? 0,
+            'verification_date' => $verificationData['vrfDate'],
+            'valid_until' => $verificationData['validDate'],
+            'is_valid' => $docs[0]['applicability'],
+        ]);
+    }
+
     private function addSlashes(string $str)
     {
-        return str_replace('-', '\-', $str);
+        return str_replace('-', '%5C-', $str);
     }
 }
