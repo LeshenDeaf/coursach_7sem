48582482c3b1e4855011dff83e4e23b14da173ba
leshen <lexuar2000@gmail.com>
2022-05-23 14:16:15 +0300

    Showing mainNumber, getting it and refreshing accruals

--- resources/views/home/partials/addresses_list.blade.php
+++ resources/views/home/partials/addresses_list.blade.php
@@ -4,6 +4,17 @@
         <div class="address">
             <div class="address_name">{{ $address->address }}</div>
             <div class="address_content">
+                @if($address->hasMainNumbers())
+                    @foreach($address->mainNumbers as $mainNumber)
+                        Main number: {{ $mainNumber->main_number }}
+                        <button class="refresh_accruals" name="{{ $mainNumber->main_number }}">
+                            Обновить показатели
+                        </button>
+                    @endforeach
+                @elseif(auth()->user()->hasEsPlusToken())
+                    <button class="get_main_number">Get ES main number</button>
+                @endif
+
                 <div class="counters_title">Counters for this address</div>
                 @include('home.partials.counters', ['counters' => $address->counters, 'address_id' => $address->id])
             </div>
