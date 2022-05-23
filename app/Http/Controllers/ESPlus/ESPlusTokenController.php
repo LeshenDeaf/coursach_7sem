8d7126fe5b650cdd54c79348d4d1fe2d55cafdaf
leshen <lexuar2000@gmail.com>
2022-05-23 13:39:37 +0300

    Created ESPlusToken controller.
    Available methods:
    * me
    * login

--- app/Http/Controllers/ESPlus/ESPlusTokenController.php
+++ app/Http/Controllers/ESPlus/ESPlusTokenController.php
@@ -3,11 +3,42 @@
 namespace App\Http\Controllers\ESPlus;
 
 use App\Http\Controllers\Controller;
+use App\Http\Services\ESPlusService;
+use App\Models\ESPlus\ESPlusToken;
+use Illuminate\Http\Request;
 
 class ESPlusTokenController extends Controller
 {
+    private ESPlusService $service;
+
+    public function __construct()
+    {
+        $this->service = new ESPlusService();
+    }
+
     public function index()
     {
         //
     }
+
+    public function store(string $refreshToken)
+    {
+        return ESPlusToken::create([
+            'user_id' => auth()->user()->id,
+            'refresh_token' => $refreshToken
+        ]);
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function login(Request $request)
+    {
+        $tokens = $this->service->login($request->input('login'), $request->input('password'));
+
+        $this->store($tokens['refresh_token']);
+
+        return $tokens;
+    }
+
 }
