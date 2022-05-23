137c52dfbd93e22db66010739aa4729b32e4fd24
leshen <lexuar2000@gmail.com>
2022-05-23 14:18:14 +0300

    Routes for esplus

--- routes/web/home/home.php
+++ routes/web/home/home.php
@@ -12,6 +12,23 @@ Route::resource(
     'fields', \App\Http\Controllers\FieldController::class
 );
 
+//Route::middleware('auth')->post(
+//    '/es_auth', [\App\Http\Controllers\ESPlus\ESPlusTokenController::class, 'store']
+//)->name('es-auth');
+
+Route::group([
+    'prefix' => 'es_plus',
+    'as' => 'es_plus',
+    'middleware' => ['cors']
+], function () {
+     Route::post('auth/login', [\App\Http\Controllers\ESPlus\ESPlusTokenController::class, 'login']);
+     Route::post('me', [\App\Http\Controllers\ESPlus\MainNumberController::class, 'me']);
+     Route::get(
+         'refresh_accruals',
+         [\App\Http\Controllers\ESPlus\AccrualsController::class, 'refresh']
+     );
+});
+
 Route::group([
     'prefix' => 'answers',
     'as' => 'answers.',
